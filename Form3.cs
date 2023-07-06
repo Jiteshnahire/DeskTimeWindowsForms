fd925e3dc6645ed619506ba9c0f0e51463887b1d
Jitesh Nahire <Jiteshnahire@gmail.com>
2023-07-06 18:38:35 +0530

    Add project files.

---
+++ Form3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeskTimeWindowsForms
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // for active
+            TimeSpan idleTime = GetSystemIdleTime();
+            label2.Text = idleTime.ToString();
+            // for inactive
+
+
+        }
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        // Structure to hold the last input information
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        public static TimeSpan GetSystemIdleTime()
+        {
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+
+            if (!GetLastInputInfo(ref lastInputInfo))
+                throw new Exception("Failed to retrieve the last input information.");
+
+            uint lastInputTime = lastInputInfo.dwTime;
+            uint currentTickCount = (uint)Environment.TickCount;
+            uint idleTime = currentTickCount - lastInputTime;
+
+            return TimeSpan.FromMilliseconds(idleTime);
+
+        }
+
+        }
+
+    }
---
+++ Form4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeskTimeWindowsForms
+{
+    public partial class Form4 : Form
+    {
+        SqlCommand cmd;
+        SqlConnection connection;
+        SqlDataReader dr;
+        public Form4()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString);
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            Form4 user_log = new Form4();
+            user_log.Show();
+            this.Hide();
+        }
+    }
+}
